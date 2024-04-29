65fcb22312378eb6fb5a97ada4c6ad93f4c8e2fc
yuetsh <517252939@qq.com>
2024-04-29 11:48:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
-    <div>{{ label }}是 {{ result }}</div>
-    <div>当前是 {{ current }}</div>
-  </div>
-  <div>
-    <input type="number" v-model="num" />
-    <select name="system" v-model="system">
-      <option value="2">2</option>
-      <option value="8">8</option>
-      <option value="16">16</option>
-    </select>
-    <button @click="start">开始转换</button>
-    <button @click="clear">清空</button>
+  <div class="container vertical-center">
+    <div class="block mb-6">
+      <div class="title is-size-1">{{ label }}是 {{ result }}</div>
+      <div class="title is-size-2">当前十进制是 {{ current }}</div>
+    </div>
+    <div class="columns mb-6">
+      <div class="column">
+        <label class="label is-large">十进制数字</label>
+        <div class="buttons">
+          <input class="input is-large" type="number" min="0" v-model="num" />
+          <button class="button is-large" @click="add">+</button>
+          <button class="button is-large" @click="minus">-</button>
+        </div>
+      </div>
+      <div class="column">
+        <label class="label is-large">选择进制</label>
+        <div class="select is-large">
+          <select name="system" v-model="system">
+            <option value="2">2</option>
+            <option value="8">8</option>
+            <option value="16">16</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="buttons">
+      <button class="button is-primary is-large" @click="start">转换</button>
+      <button class="button is-large" @click="clear">清空</button>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -55,4 +71,36 @@ function clear() {
   current.value = 0
   result.value = "0"
 }
+
+function add() {
+  num.value += 1
+}
+
+function minus() {
+  if (num.value > 0 && num.value > current.value) {
+    num.value -= 1
+  }
+}
 </script>
+<style scoped>
+@font-face {
+  font-family: "MyFont";
+  src: url(/font.ttf);
+}
+
+* {
+  font-family: "MyFont";
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 80%;
+}
+
+.vertical-center {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
